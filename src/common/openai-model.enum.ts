29f2d1076d79f7d6d437567de26889c7fb0b270b
Arvin Kent Lazaga <arvinkent121816@gmail.com>
2025-06-06 16:29:30 +0800

    Added OpenAI model enum

---
+++ src/common/openai-model.enum.ts
@@ -0,0 +1,16 @@
+export enum OpenAIModel {
+  GPT_3_5_TURBO = "gpt-3.5-turbo",
+  GPT_3_5_TURBO_16K = "gpt-3.5-turbo-16k",
+  GPT_4 = "gpt-4",
+  GPT_4_TURBO = "gpt-4-turbo",
+  GPT_4O = "gpt-4o",
+  TEXT_DAVINCI_003 = "text-davinci-003",
+  TEXT_CURIE_001 = "text-curie-001",
+  TEXT_BABBAGE_001 = "text-babbage-001",
+  TEXT_ADA_001 = "text-ada-001",
+  CODE_DAVINCI_002 = "code-davinci-002",
+  TEXT_EMBEDDING_ADA_002 = "text-embedding-ada-002",
+  WHISPER_1 = "whisper-1",
+  DALLE_2 = "dall-e-2",
+  DALLE_3 = "dall-e-3"
+}
